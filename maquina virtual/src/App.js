51aea5e072ba14cc55d4e9aca597a9648bb5955e
Evandro Capovilla <vandinhocapovilla@live.com>
2019-08-18 17:50:52 -0300

    pilha and function

--- maquina virtual/src/App.js
+++ maquina virtual/src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Icon from '@material-ui/core/Icon';
 import './App.css';
 import Tela from './components/tela/Tela'
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-  leftIcon: {
-    marginRight: theme.spacing(1),
-  },
-  rightIcon: {
-    marginLeft: theme.spacing(1),
-  },
-  iconSmall: {
-    fontSize: 20,
-  },
-}));
-
 function App() {
-  const classes = useStyles();
   return (
     <Tela />
   );
--- maquina virtual/src/components/tela/Tela.js
+++ maquina virtual/src/components/tela/Tela.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
-import Paper from '@material-ui/core/Paper'
 import Divider from '@material-ui/core/Divider'
 import { makeStyles } from '@material-ui/core/styles';
 import Menu from '../menu/Menu'
+import Funcao from '../funcao/Funcao'
+// import Pilha from '../pilha/Pilha'
+
 
 export default function CSSGrid() {
     return (
@@ -16,9 +17,11 @@ export default function CSSGrid() {
                 <Grid container spacing={3}>
                     <Grid item xs={8}>
                         Instruções a serem executadas pela MV
+                        <Funcao />
                     </Grid>
                     <Grid item xs={4}>
                         Conteúdo da pilha
+                        {/* <Pilha /> */}
                     </Grid>
                 </Grid>
             </Container>
