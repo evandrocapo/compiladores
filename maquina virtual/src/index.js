448dfcf82d918dab8e6995de21ca57a2709adc20
Evandro Capovilla <evandrocapovillajr@gmail.com>
2019-10-26 19:48:15 -0300

    axios ordem

--- maquina virtual/src/index.js
+++ maquina virtual/src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
 axios.defaults.baseURL = 'http://localhost:3000';
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
