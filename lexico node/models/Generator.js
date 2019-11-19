328b8cfd920f9b2c4838bb9ac7dc19f8a51015b5
Evandro Capovilla <evandro.junior@venturus.org.br>
2019-11-18 21:38:25 -0300

    add getCodigo, generateCode and gera

--- lexico node/models/Generator.js
+++ lexico node/models/Generator.js
@@ -1,22 +1,114 @@
-class Generator{
+var fs = require('fs');
+
+class Generator {
 
     constructor() {
         this.codigo = new Array;
     }
 
-    gera(rotulo,comando,param1,param2){ 
+    gera(label, command, param1, param2) {
 
-        //jmp = command null
-
-
-       // geraCode = ; // string
-        this.codigo.push(geraCode);
+        if (label) {
+            this.codigo.push("L" + label);
+        }
+        else {
+            switch (command) {
+                case 'JMP':
+                    label_ = "L" + param1;
+                    this.codigo.push(command + " " + label_) // JMP L1
+                    break;
+                case 'JMPF':
+                    label_ = "L" + param1;
+                    this.codigo.push(command + " " + label_) // JMP L1
+                    break;
+                case 'START':
+                    this.codigo.push(command)
+                    break;
+                case 'ALLOC':
+                    this.codigo.push(command + " " + param1); // ALLOC m
+                    break;
+                case 'DALLOC':
+                    this.codigo.push(command + " " + param1); // DALLOC m
+                    break;
+                case 'CALL':
+                    this.codigo.push(command + " " + param1); // CALL p
+                    break;
+                case 'RETURN':
+                    this.codigo.push(command); // RETURN
+                    break;
+                case 'RETURNF':
+                    this.codigo.push(command); // RETURNF
+                    break;
+                case 'RD':
+                    this.codigo.push(command + " " + param1); // RD não tem parametros porém precisamos ler do teclado algo.
+                    break;
+                case 'PRN':
+                    this.codigo.push(command); // PRN
+                    break;
+                case 'HLT':
+                    this.codigo.push(command); // HLT
+                    break;
+                case 'LDC':
+                    this.codigo.push(command + " " + param1); // LDC k
+                    break;
+                case 'LDV':
+                    this.codigo.push(command + " " + param1) // LDV n
+                    break;
+                case 'ADD':
+                    this.codigo.push(command); // ADD
+                    break;
+                case 'SUB':
+                    this.codigo.push(command); // SUB
+                    break;
+                case 'MULT':
+                    this.codigo.push(command); // MULT
+                    break;
+                case 'DIVI':
+                    this.codigo.push(command); // DIVI
+                    break;
+                case 'INV':
+                    this.codigo.push(command); // INV
+                    break;
+                case 'AND':
+                    this.codigo.push(command); 
+                    break;
+                case 'OR':
+                    this.codigo.push(command); 
+                    break;
+                case 'NEG':
+                        this.codigo.push(command);
+                    break;
+                case 'CME':
+                        this.codigo.push(command);
+                    break;
+                case 'CMA':
+                        this.codigo.push(command);
+                    break;
+                case 'CEQ':
+                        this.codigo.push(command);
+                    break;
+                case 'CDIF':
+                        this.codigo.push(command);
+                    break;
+                case 'CMEQ':
+                        this.codigo.push(command);
+                    break;
+                case 'CMAQ':
+                        this.codigo.push(command);
+                    break;
+            }
+        }
     }
 
-    getCodigo(){
+    getCodigo() {
         return this.codigo;
+    }
+
+    generateCode(){
+        try { fs.writeFileSync('code.obj', this.codigo, 'utf-8'); }
+        catch(e) { alert('Impossivel salvar !'); }
     }
 
 }
 
-module.exports = {Generator}
+module.exports = { Generator }
